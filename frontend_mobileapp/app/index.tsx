68c921351025925c9a17748a29fbf06a5abf2981
shemanto27 <shemantosharkarofficial@gmail.com>
2026-02-23 07:31:37 +0600

    Initial commit

---
+++ frontend_mobileapp/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="bg-green-300">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
